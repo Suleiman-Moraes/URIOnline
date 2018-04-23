b3ea55cad2e9548b2019570c319773cab11e4ade
suleiman-am <suleiman-am@c-3804.agr.go>
2018-04-23 09:34:57 -0300

    Atualização

--- URIOnline/src/exercicios/ex1036/Main.java
+++ URIOnline/src/exercicios/ex1036/Main.java
@@ -1,22 +1,31 @@
 package exercicios.ex1036;
 
+import java.util.Formatter;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		int[] num = new int[4];
-		String x = leia.nextLine();
-		String[] vet = x.replaceAll(" ", "#").split("#");
-		for (int i = 0; i < vet.length; i++) {
-			num[i] = Integer.parseInt(vet[i]);
-		}
-		
-		if(num[1] > num[2] && num[3] > num[0] && (num[2] + num[3]) > (num[0] + num[1]) && num[2] >= 0 && num[3] >= 0 && num[0] % 2 == 0){
-			System.out.println("Valores aceitos");
+		String t = leia.nextLine();
+		String[] vet = t.split(" ");
+		double a = Double.parseDouble(vet[0]);
+		double b  = Double.parseDouble(vet[1]);
+		double c = Double.parseDouble(vet[2]);
+		if(a == 0){
+			System.out.println("Impossivel calcular");
 		}else{
-			System.out.println("Valores nao aceitos");
+			double d = Math.pow(b, 2) - (4 * a * c);
+			if(d < 0){
+				System.out.println("Impossivel calcular");
+			}else{
+				Formatter fmt = new Formatter(System.out);
+				double b1 = (-b + Math.sqrt(d)) / (2 * a);
+				double b2 = (-b - Math.sqrt(d)) / (2 * a);
+				fmt.format("R1 = %.5f\n", b1);
+				fmt.format("R2 = %.5f\n", b2);
+				fmt.close();
+			}
 		}
 		
 		leia.close();
