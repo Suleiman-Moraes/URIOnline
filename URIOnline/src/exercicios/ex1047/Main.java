edbb74b98783daff7ee329d36909f01c2969e0db
suleiman-am <suleiman-am@c-3804.agr.go>
2018-04-27 09:05:23 -0300

    URI

--- URIOnline/src/exercicios/ex1047/Main.java
+++ URIOnline/src/exercicios/ex1047/Main.java
@@ -4,25 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		int h1, m1, h2, m2;
-		Scanner leia = new Scanner(System.in);
-		h1 = leia.nextInt();
-		m1 = leia.nextInt();
-		h2 = leia.nextInt();
-		m2 = leia.nextInt();
-		leia.close();
-		
-		int soma = 0;
-		
-		if(a > b){
-			soma = 24 - a + b;
-			System.out.println("O JOGO DUROU " + soma + " HORA(S)");
-		}else if(a < b){
-			soma = b - a;
-			System.out.println("O JOGO DUROU " + soma + " HORA(S)");
-		}else{
-			System.out.println("O JOGO DUROU 24 HORA(S)");
-		}
-	}
+    public static void main(String[] args){
+
+        int hora, horaF, minuto, minutoF, duracaoH, duracaoM;
+        Scanner sc = new Scanner(System.in);
+
+        hora = sc.nextInt();
+        minuto = sc.nextInt();
+        horaF = sc.nextInt();
+        minutoF = sc.nextInt();
+        sc.close();
+        
+        duracaoH = horaF - hora;
+        if (duracaoH < 0) {
+            duracaoH = 24 + (horaF - hora);
+        }
+        duracaoM = minutoF - minuto;
+        if (duracaoM < 0) {
+            duracaoM = 60 + (minutoF - minuto);
+            duracaoH--;
+        }
+        if (hora == horaF && minuto == minutoF) {
+            System.out.print("O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)\n");
+        } else {
+            System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", duracaoH, duracaoM);
+        }
+    }
+
 }
