276f3909e02f5be1f85aa0a44f2e8fefc06ea020
suleiman-am <suleiman-am@c-3804.agr.go>
2018-05-15 11:49:56 -0300

    Atualiza

---
+++ URIOnline/src/exercicios/ex1072/Main.java
@@ -0,0 +1,32 @@
+package exercicios.ex1072;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Locale.setDefault(Locale.US);
+//		int a, b;
+//		Scanner leia = new Scanner(System.in);
+//		a = leia.nextInt();
+//		b = leia.nextInt();
+//		leia.close();
+//		
+//		int soma = 0;
+//		if(a < b){
+//			for (int i = (a + 1); i < b; i++) {
+//				if(i % 2 != 0){
+//					soma += i;
+//				}
+//			}
+//		}
+//		else{
+//			for (int i = (b + 1); i < a; i++) {
+//				if(i % 2 != 0){
+//					soma += i;
+//				}
+//			}
+//		}
+//		System.out.println(soma);
+	}
+}
