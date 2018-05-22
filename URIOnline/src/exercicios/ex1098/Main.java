d6ac7f02a91c43b9af8272603e4741463d374f92
suleiman-am <suleiman-am@c-3804.agr.go>
2018-05-22 09:10:47 -0300

    1098

---
+++ URIOnline/src/exercicios/ex1098/Main.java
@@ -0,0 +1,21 @@
+package exercicios.ex1098;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+public class Main {
+	public static void main(String[] args) {
+		NumberFormat dec = new DecimalFormat("#.#");
+//		 Locale.setDefault(Locale.US);
+//		StringBuffer tudo = new StringBuffer();
+		float i = 0;
+		float j = 1;
+		do {
+			for (int k = 0; k < 3; k++) {
+				System.out.println("I=" + dec.format(i) + " J=" + dec.format((j + k)));
+			}
+			j += 0.2;
+			i += 0.2;
+		}while (i <= 2.1);
+	}
+}
