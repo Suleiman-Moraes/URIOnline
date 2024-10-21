c62f36c6b1829011f3abe20929d00eb61e66f89f
suleiman-moraes <suleimanmoraes@gmail.com>
2024-10-21 12:06:45 +0100

    ex2160

---
+++ src/exercicios/iniciante/ex2160/MainTest.java
@@ -0,0 +1,15 @@
+package exercicios.iniciante.ex2160;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void testExecute() {
+		assertEquals("YES", Main.execute("Fulano de Tal"));
+		assertEquals("NO", Main.execute(
+				"Pedro de Alcantara Francisco Antonio Joao Carlos Xavier de Paula Miguel Rafael Joaquim Jose Gonzaga Pascoal Cipriano Serafim"));
+	}
+}
---
+++ src/exercicios/iniciante/ex2161/Main.java
@@ -0,0 +1,35 @@
+package exercicios.iniciante.ex2161;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * beecrowd | 2160 Nome no Formulário Por M.C. Pinto, UNILA BR Brazil
+ * 
+ * Timelimit: 1 Preencher formulários é uma tarefa simples. Mas é preciso
+ * conferir se o espaço reservado para os dados é suficiente.
+ * 
+ * Sua tarefa é, dada uma linha de texto, indicar se ele cabe ou não cabe em um
+ * formulário com 80 caracteres.
+ * 
+ * Entrada A entrada é uma linha de texto L (1 ≤ |L| ≤ 500).
+ * 
+ * Saída A saída é dada em uma única linha. Ela deve ser "YES" (sem as aspas) se
+ * a linha de texto L tem até 80 caracteres. Se L tem mais de 80 caracteres, a
+ * saída deve ser "NO".
+ * 
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		System.out.println(execute(scan.nextInt()));
+		scan.close();
+	}
+
+	protected static String execute(int value) {
+		
+		return text.length() <= 80 ? "YES" : "NO";
+	}
+}
